b07fe085bf168fcf9bfcc581c61bc25a5e50dd67
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2024-06-15 23:00:34 -0400

    add constant variable

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import process from 'node:process';
 import { updateState, getState } from '../db/state.js';
 import CheckBattery from './CheckBattery.js';
 
+const RUNNING = true;
+
 const checkState = async () => {
   // Log a message when a signal is received
   CheckBattery.log({ isFirst: false, message: 'SIG received.' });
@@ -29,5 +31,7 @@ process.on('exit', (code) => {
   console.log(`Program exiting with code ${code}`);
 });
 
-// Start the main function of your program
-CheckBattery.main();
+// Start the main function of my program
+if (RUNNING) {
+  CheckBattery.main();
+}
